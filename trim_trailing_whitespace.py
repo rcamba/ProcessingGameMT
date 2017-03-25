732defe313f473f785e3be29987b34e26b2ab3fb
Robert <robertCamba@gmail.com>
2017-03-25 14:45:21 -0500

    trim all files in folder based on first arg passed

--- trim_trailing_whitespace.py
+++ trim_trailing_whitespace.py
@@ -1,17 +1,17 @@
 import os
-targ_dir = "g"
+import sys
+targ_dir = sys.argv[1]
 f_list = os.listdir(targ_dir)
 for f in f_list:
 	full_path = os.path.join(targ_dir, f)
-	
+
 	with open(full_path) as file_:
 	    lines = file_.readlines();
-	    for i in range(0, len(lines)):
-	    	lines[i] = lines[i].rstrip() 
+        for i in range(0, len(lines)):
+	        lines[i] = lines[i].rstrip()
 
 	if len(lines) > 0:
 		with open(full_path, 'w') as writer:
 			for l in lines:
 				writer.write(l)
 				writer.write("\n")
-	        
